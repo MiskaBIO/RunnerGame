235451595e40336587f45f298051b07974409ff9
MiskaBIO <schorschova.michaela@gmail.com>
2022-01-30 22:19:22 +0100

    Walking animation and game over scene
    Signed-off-by: MiskaBIO <schorschova.michaela@gmail.com>

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import pygame
 from sys import exit
 
+
+def display_score():
+    current_time = pygame.time.get_ticks() - start_time
+    score_font = pygame.font.SysFont("Verdana", 30)
+    score = '{0:.1f}'.format(current_time / 1000)
+    score_surface = score_font.render(f"Score: {score} ", False, "Black")
+    score_rect = score_surface.get_rect(center=(650, 50))
+    pygame.draw.rect(screen, "Pink", score_rect)
+    pygame.draw.rect(screen, "Pink", score_rect, 10)
+    screen.blit(score_surface, score_rect)
+    return score
+
+
 if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode((800, 400))
     pygame.display.set_caption("Wild runner")
     clock = pygame.time.Clock()
     game_active = True
+    start_time = 0
+    score = '{0:.1f}'.format((pygame.time.get_ticks() - start_time)/1000)
 
     # background
     sky_surface = pygame.image.load("images/bg_desert.png").convert()
@@ -15,17 +30,9 @@ if __name__ == '__main__':
     text_rect = text_surface.get_rect(midbottom=(400, 70))
 
     # score
-    score_font = pygame.font.SysFont("Verdana", 30)
-    score_surface = score_font.render("Score:", False, "Black")
-    score_rect = score_surface.get_rect(center=(700, 50))
-
-    # Game over message
-    Game_Over_font = pygame.font.SysFont("Helvetica", 40)
-    Game_Over_surface = Game_Over_font.render("Game Over", False, "Black")
-    Game_Over_rect = Game_Over_surface.get_rect(center=(400, 200))
-    message_font = pygame.font.SysFont("Verdana", 20)
-    message_surface = message_font.render("to play again press Space", False, "Grey")
-    message_rect = message_surface.get_rect(center=(400, 120))
+    #score_font = pygame.font.SysFont("Verdana", 30)
+    #score_surface = score_font.render( f"Score: {current_time} s.", "Black")
+    #score_rect = score_surface.get_rect(center=(700, 50))
 
     # snail
     snail_surface = pygame.image.load("images/snailWalk1.png").convert_alpha()
@@ -33,6 +40,7 @@ if __name__ == '__main__':
 
     # player
     player_surface = pygame.image.load("images/p1_walk01.png").convert_alpha()
+    player_surface2 = pygame.image.load("images/p1_stand.png").convert_alpha()
     player_rect = player_surface.get_rect(midbottom=(80, 300))
     player_gravity = 0
 
@@ -51,11 +59,15 @@ if __name__ == '__main__':
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE and player_rect.bottom == 300:
                         player_gravity = -20
-                else:
-                    if event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_SPACE:
-                            game_active = True
-                            snail_rect.left = 800
+
+
+            else:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        game_active = True
+                        snail_rect.left = 800
+                        player_rect.left = 0
+                        start_time = pygame.time.get_ticks()
 
             # if event.type == pygame.KEYUP:
             #   pass
@@ -64,13 +76,12 @@ if __name__ == '__main__':
             pygame.draw.rect(screen, (110, 176, 210), text_rect)
             pygame.draw.rect(screen, (110, 176, 210), text_rect, 8)
             screen.blit(text_surface, text_rect)
-            pygame.draw.rect(screen, "Pink", score_rect)
-            pygame.draw.rect(screen, "Pink", score_rect, 10)
-            screen.blit(score_surface, score_rect)
+
             pygame.draw.line(screen, "Black", (0, 300), (800, 300), 10)
             # pygame.draw.ellipse(screen, "Brown", pygame.Rect(50, 200, 100, 100))
+            score = display_score()
 
-            snail_rect.left -= 3
+            snail_rect.left -= 2
             if snail_rect.right <= 0:
                 snail_rect.right = 800
             screen.blit(snail_surface, snail_rect)
@@ -81,13 +92,31 @@ if __name__ == '__main__':
             if player_rect.bottom >= 300:
                 player_rect.bottom = 300
 
-            player_rect.left += 3
+            player_rect.left += 5
             if player_rect.left >= 800:
                 player_rect.left = 0
-            screen.blit(player_surface, player_rect)
+
+            if player_rect.left % 80 > 40:
+                screen.blit(player_surface, player_rect)
+            else:
+                screen.blit(player_surface2, player_rect)
 
             if player_rect.colliderect(snail_rect):
                 game_active = False
+                screen.fill((94, 124, 162))
+
+                # Game over message
+                player_stand = pygame.image.load("images/p1_stand.png").convert_alpha()
+                player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
+                player_stand_rect = player_stand.get_rect(center=(400, 200))
+                Game_Over_font = pygame.font.SysFont("Helvetica", 40)
+                Game_Over_surface = Game_Over_font.render(f"Game Over! Your score: {score}", False, "Black")
+                Game_Over_rect = Game_Over_surface.get_rect(center=(400, 90))
+                message_font = pygame.font.SysFont("Verdana", 20)
+                message_surface = message_font.render("to play again press Space", False, "Grey")
+                message_rect = message_surface.get_rect(center=(400, 300))
+
+                screen.blit(player_stand, player_stand_rect)
                 screen.blit(Game_Over_surface, Game_Over_rect)
                 screen.blit(message_surface, message_rect)
                 if event.type == pygame.KEYDOWN:
